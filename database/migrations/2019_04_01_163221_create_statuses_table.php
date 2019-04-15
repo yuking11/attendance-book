78bcba873b19c3bcbdc3c1233705934a7ec0d962
yuking11 <ys11.works@gmail.com>
2019-04-15 12:45:12 +0900

    テーブル名変更

--- database/migrations/2019_04_01_163221_create_statuses_table.php
+++ database/migrations/2019_04_01_163221_create_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatusTable extends Migration
+class CreateStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateStatusTable extends Migration
      */
     public function up()
     {
-        Schema::create('status', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->bigInteger('member_id')->unsigned();
             $table->bigInteger('calendar_id')->unsigned();
-            $table->boolean('state');
+            $table->boolean('status');
             $table->timestamps();
 
             $table->foreign('member_id')->references('id')->on('members');
