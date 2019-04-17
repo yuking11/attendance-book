9415fcf479242783f5269fcdcd80e842c3411169
yuking11 <ys11.works@gmail.com>
2019-04-17 16:44:08 +0900

    タグマネ追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,13 @@
   <meta name="format-detection" content="telephone=no">
   <title>{{ config('app.name') }}</title>
   <meta name="description" content="Web上で出席簿をつけられるよ。">
+    <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5T5N3DB');</script>
+  <!-- End Google Tag Manager -->
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p:400,500,700" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" rel="stylesheet">
@@ -16,6 +23,10 @@
   <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body class="l-body">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T5N3DB"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <div id="app"></div>
 </body>
 </html>
