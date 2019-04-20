7a54f9505360fff350210b38c15bd128857bee7e
yuking11 <ys11.works@gmail.com>
2019-04-20 15:32:01 +0900

    エラーページ デザイン修正

--- resources/js/pages/errors/NotFound.vue
+++ resources/js/pages/errors/NotFound.vue
@@ -1,3 +1,12 @@
 <template>
-  <p>お探しのページは見つかりませんでした。</p>
+  <div class="l-content-small">
+    <div class="l-inner">
+
+      <h1 class="c-ttl-primary">404 Not Found</h1>
+      <p class="u-mb1e">お探しのページは見つかりませんでした。</p>
+
+      <p class="u-mb1e">アクセスしようとしたページは存在しない／削除された／URLが変更されているため、見つけることができません。</p>
+
+    </div>
+  </div>
 </template>
--- resources/js/util.js
+++ resources/js/util.js
@@ -1,6 +1,7 @@
 export const OK = 200
 export const CREATED = 201
 export const NO_CONTENT = 204
+export const FORBIDDEN = 403
 export const NOT_FOUND = 404
 export const CONFLICT = 409
 export const UNAUTHORIZED = 419
